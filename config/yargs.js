81d9d92ec64cd001d7b7bc7cd888269a8e346369
Oniel <edwinavilez@hotmail.com>
2020-04-12 12:21:40 -0600

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,25 @@
+const descripcion = {
+    alias: 'd',
+    demand: true
+}
+
+const completado = {
+    alias: 'c',
+    default: true
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('borrar', 'Borrar tarea por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    }).help().argv;
+
+module.exports = {
+    argv
+}
